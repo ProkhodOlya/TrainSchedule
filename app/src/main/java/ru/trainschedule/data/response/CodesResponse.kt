8630ec9327232087f2145d30cbd319d62e5d129f
Olla <prohoda_o@mail.ru>
2024-09-15 20:31:10 +0300

    add StationResponse

---
+++ app/src/main/java/ru/trainschedule/data/response/CodesResponse.kt
@@ -0,0 +1,5 @@
+package ru.trainschedule.data.response
+
+internal class CodesResponse(
+    val yandexCode : String
+)
---
+++ app/src/main/java/ru/trainschedule/data/response/StationResponse.kt
@@ -0,0 +1,13 @@
+package ru.trainschedule.data.response
+
+internal class StationResponse(
+    val direction: String,
+    val codes: CodesResponse,
+    val stationType: StationTypeResponse,
+    val title: String,
+    val longitude: Float,
+    val transportType: TransportTypeResponse,
+    val latitude: Float,
+    val esrtCode: String,
+) {
+}
