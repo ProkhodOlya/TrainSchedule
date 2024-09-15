25cad121ab2e08a241da27f164676932d20766aa
Olla <prohoda_o@mail.ru>
2024-09-15 20:58:38 +0300

    add serialization to Response

--- app/src/main/java/ru/trainschedule/data/response/CodesResponse.kt
+++ app/src/main/java/ru/trainschedule/data/response/CodesResponse.kt
@@ -1,5 +1,12 @@
 package ru.trainschedule.data.response
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 internal class CodesResponse(
+    @SerialName ("esr_codes")
+    val esrCodes: String,
+    @SerialName ("yandex_code")
     val yandexCode : String
 )
--- app/src/main/java/ru/trainschedule/data/response/TransportTypeResponse.kt
+++ app/src/main/java/ru/trainschedule/data/response/TransportTypeResponse.kt
@@ -1,10 +1,20 @@
 package ru.trainschedule.data.response
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
 internal enum class TransportTypeResponse {
+    @SerialName("plane")
     PLANE,
+    @SerialName("train")
     TRAIN,
+    @SerialName("suburban")
     SUBURBAN,
+    @SerialName("bus,")
     BUS,
+    @SerialName("water")
     WATER,
+    @SerialName("helicopter")
     HELICOPTER,
 }
